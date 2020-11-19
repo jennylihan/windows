fadf51ee82945ba29bf2e8279bb2546d35010557
Glitch (hello-webpage) <none>
2020-11-19 12:15:23 +0000

    🗺⛄️ Checkpoint ./sketch.js:5753059/4657 ./index.html:5753059/3493 ./index.css:5753059/1375

--- sketch.js
+++ sketch.js
@@ -1,14 +1,16 @@
 var x = 0;
 var y = 0;
 let emotions = [];
+let displayRussell = false;
+let RussellList = [[50, 50, '12 am'], [50, 70, 'heyo']];
 
 function setup() {
   background(0);
   // let inp = createInput('');
   // inp.input(myInputEvent);
   createCanvas(windowWidth, windowHeight);
-  textSize(20);
-  textAlign(CENTER, CENTER);
+  textSize(25);
+  textAlign(LEFT, CENTER);
   x = windowWidth/2;
   y = windowHeight/2;
 }
@@ -16,30 +18,39 @@ function setup() {
 function draw() {
   background('black');
   fill('white');
-  text('pleasant →', windowWidth *.95, windowHeight/2);
-  text('← miserable', windowWidth *.05, windowHeight/2);
-  text('↑ high energy', windowWidth/2, windowHeight*0.05);
-  text('↓ low energy', windowWidth/2, windowHeight*0.95);
+  text('pleasant →', windowWidth *.91, windowHeight/2);
+  text('← miserable', windowWidth * 0.01, windowHeight/2);
+  text('↑ high energy', windowWidth/2 - 50, windowHeight*0.025);
+  text('↓ low energy', windowWidth/2 - 50, windowHeight*0.965);
+  text('ARROW KEYS to move', windowWidth*.01, 20)
+  text('SPACE BAR to mark an emotional spot', windowWidth*.01, 40)
+  text('TAB to toggle circumplex labels from (Russell, 1980)', windowWidth * .01, 60)
 
+  drawEmotions(emotions);
+  if (displayRussell){
+    drawEmotions(RussellList);
+  }
   fill(mapColor(x, y));
   ellipse(x,y,100,100);
   checkIfOutOfBounds();
-
-  emotions.forEach(function(item, index, array) {
-    x1 = item[0];
-    y1 = item[1];
-    // ellipse(x1, y1,50, 50);
-    fill(mapColor(x1,y1))
-    text(item[2], x1, y1);
-  })
   checkKeys();
+}
+
+function drawEmotions(emotionslist){
+  emotionslist.forEach(function(item, index, array) {
+    x1 = map(item[0], 0, 100, 0, windowWidth);
+    y1 = map(item[1], 0, 100, 0, windowHeight);
+    // ellipse(x1, y1,50, 50);
+    fill(mapColor(x1,y1));
+    text(item[2], x1, y1);
+  });
 }
 
 function mapColor(x, y){
   colorMode(HSB, 100);
-  h = 5 + 95*(x/windowWidth);
-  s = 5 + 95*(y/windowHeight);
-  b = 5 + 95* (1 - y/windowHeight);
+  h = 100*(x/windowWidth);
+  s = 100*(y/windowHeight);
+  b = 100* (1 - y/windowHeight);
   return color(h, s, b);
 }
 
@@ -55,24 +66,21 @@ function checkKeys(){
   if (keyIsPressed && keyCode === 32) {
     keyCode = 33;
     var txt;
-    var emotion = prompt("Please enter your feels:", "joy");
+    var emotion = prompt("Mark this spot with a word:", "joy");
     if (emotion == null || emotion == "") {
       txt = "ok nvm";
     } else {
       txt = emotion;
     }
-    emotions.push([x, y, txt]);
+    newx = map(x, 0, windowWidth, 0, 100);
+    newy = map(y, 0, windowHeight, 0, 100)
+    emotions.push([newx, newy, txt]);
+  }
+  if (keyIsPressed && keyCode === TAB) {
+    keyCode = 0;
+    displayRussell = !displayRussell;
   }
 }
-
-// function myInputEvent() {
-//   console.log('you are typing: ', this.value());
-//
-//   if (keyIsPressed && keyCode === 32) {
-//     emotions.push([x, y, this.value()]);
-//     alert("do you want to mark this spot as " + this.value() +"?");
-//   }
-// }
 
 function keyPressed() {
   //code adapted from https://editor.p5js.org/2sman/sketches/rkGp1alib
@@ -87,8 +95,3 @@ function keyPressed() {
     x = x + 50;
   }
 }
-
-// other color scheme options:
-// r = 255*(x/windowWidth);
-// g = 255*(1 - y/windowHeight);
-// b = 255* (1 - y/windowHeight)
