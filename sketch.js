17713283640fbe174837824ce7b912abef682a78
Glitch (hello-webpage) <none>
2020-11-21 04:06:25 +0000

    👁🍀 Checkpoint ./sketch.js:5753059/5

--- sketch.js
+++ sketch.js
@@ -24,7 +24,7 @@ class Particle {
   constructor(){
     this.x = random(0,windowWidth);
     this.y = random(0,windowHeight);
-    this.r = random(1,8);
+    this.r = random(2,10);
     this.xSpeed = random(-2,2);
     this.ySpeed = random(-1,1.5);
   }
