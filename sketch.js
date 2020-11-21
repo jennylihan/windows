f21da6361df49aaaa288524d9fee0f22bfe5ee88
Glitch (hello-webpage) <none>
2020-11-21 05:26:01 +0000

    🐡👗 Checkpoint ./index.html:5753059/313 ./.glitch-assets:5753059/364 ./sketch.js:5753059/197

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 let notosans;
 function preload() {
   notosans = loadFont(
-    "https://cdn.glitch.com/d343bc20-d576-4fcf-8829-86baa7d563d6%2FNotoSansSC-Black.otf?v=1605935481448"
+    "https://cdn.glitch.com/d343bc20-d576-4fcf-8829-86baa7d563d6%2FNotoSansSC-Medium.otf?v=1605936141444"
   );
 }
 
