29f995f441a8c3d6a3fbc5126db2fa2fc07d077d
Glitch (space-to-feel) <none>
2021-03-15 03:25:44 -0700

    link window 1 and window2

--- window2.js
+++ window2.js
@@ -3,41 +3,27 @@ let windowlist = new Set();
 let starlist = [];
 let moonlist = [];
 let unit = 100;
-let max_ring = 3; //needs to be odd number
+let max_ring = 7; //needs to be odd number
 let moon;
 let center_x = 0;
 let center_y = 0;
 let strokeweight = 30;
+let ang = 0;
 
 function setup() {
   angleMode(DEGREES);
   cnv = createCanvas(windowWidth, windowHeight, WEBGL);
   regenerate();
   img = loadImage('moon1.png');
-  //moon
-  moonlist.push(new Moon(true));
-  moonlist.push(new Moon(false));
+
 }
 
 function draw() {
   background('black');
-  //night scene with stars from http://blog.ocad.ca/wordpress/digf6003-fw201803-01/2019/02/computational-apis-moon-phases-when/
   fill('#030362');
   rectMode(CENTER);
   square(center_x, center_y, max_ring*unit);
 
-  //ring of window panes
-  windowlist.forEach(function(item){
-    item.display();
-  });
-
-  //draw moon
-  moonlist.forEach(function(item){
-    item.update();
-    item.display();
-  });
-  //clean up spill over
-  clean_up_spill();
 }
 
 function regenerate(){
@@ -46,12 +32,6 @@ function regenerate(){
   //create a window
   for (let i = 1; i  <= max_ring; i+=2){
     windowlist.add(new Ring(i));
-  }
-  //make star list
-  for (var i = 0; i < 50*max_ring; i++) {
-    var x = random(-1*max_ring*unit/2, max_ring*unit/2);
-    var y = random(-1*max_ring*unit/2, max_ring*unit/2);
-    starlist.push([x,y]);
   }
 }
 
@@ -79,32 +59,6 @@ function inbounds(x, y, width,height){
   return (x < width/2 && y < height/2 && x > -1*width/2 && y > -1*height/2);
 }
 
-
-class Moon {
-  constructor(xmouse){
-    this.r = 3*unit;
-    this.x = -5*unit;
-    this.y = 3*unit;
-    this.yoffset = -3*unit;
-    this.angle = 0;
-    this.mouse = xmouse;
-  }
-  update() {
-    this.angle += this.mouse ?  map(mouseX, 0, width, 0, 1) : map(mouseY, 0, height, -1, 0) ;
-    this.x = this.r*cos(this.angle);
-    this.y = this.r*sin(this.angle) + this.yoffset;
-  }
-  display() {
-    rotate(this.angle);
-    if (this.mouse){
-      draw_stars();
-    }
-    if (inbounds(this.x, this.y, max_ring*unit,max_ring*unit)){
-      image(img, this.x, this.y, unit, unit);
-    }
-    rotate(-1*this.angle);
-  }
-}
 
 class Ring {
   constructor(ring){
@@ -141,10 +95,10 @@ class Ring {
     square(this.x, this.y, length);
   }
 }
-function mouseClicked(){
-   window.location = 'window2.html';
-}
 
+function mouseClicked(){
+   window.location = 'march12.html';
+}
 function keyPressed() {
   if (keyCode === UP_ARROW) {
     max_ring +=2;
