05eb7e5d57943bc5a8bd77298c51d6b62ffbea17
Glitch (drawingwindows) <none>
2021-10-21 09:14:31 +0000

    added home page and about page

--- window2.js
+++ window2.js
@@ -1,4 +1,11 @@
-let notosans;let img;
+let notosans;
+function preload() {
+  notosans = loadFont(
+    "https://cdn.glitch.com/d343bc20-d576-4fcf-8829-86baa7d563d6%2FNotoSansSC-Regular.otf?v=1605939143378"
+  );
+}
+
+let imglist = [];
 let windowlist = new Set();
 let starlist = [];
 let moonlist = [];
@@ -10,9 +17,12 @@ let center_y = 0;
 let strokeweight = 30;
 let ang = 0;
 let bgcol = 'firebrick';
+let imgtracker = 0;
 let alpha_list = [255,255,240, 15, 230,240, 225,0];
 let max_random = 5;
 let timer = 0; //every 4 seconds
+let cam;
+let backdrop;
 
 function setup() {
   mouseX = 0;
@@ -21,6 +31,11 @@ function setup() {
   cnv = createCanvas(windowWidth, windowHeight, WEBGL);
   regenerate();
   cam = createCapture(VIDEO);
+  textFont(notosans);
+  textAlign(CENTER);
+
+  
+  imglist = [cam, loadImage("https://cdn.glitch.com/32b5db6a-e476-4e16-aec6-aacf0ab94b13%2FIMG_861D1B6658A3-1.jpeg?v=1615954151220"), loadImage("https://cdn.glitch.com/32b5db6a-e476-4e16-aec6-aacf0ab94b13%2FIMG_1515.JPG?v=1615958627171"), loadImage("https://cdn.glitch.com/32b5db6a-e476-4e16-aec6-aacf0ab94b13%2FP_20150714_201423_LL.jpg?v=1615958631103"), loadImage("https://cdn.glitch.com/32b5db6a-e476-4e16-aec6-aacf0ab94b13%2FPalette%20(11).png?v=1615958725789")];
   let side = unit*max_ring;
   cam.size(side, side);
   cam.hide();
@@ -29,20 +44,35 @@ function setup() {
 function draw() {
   background(bgcol);
   rectMode(CENTER);
-  texture(cam);
+  backdrop = imglist[imgtracker % imglist.length];  
+  //then scale it by -1 in the x-axis
+  //to flip the image
+  scale(-1, 1);
+  texture(backdrop);
   square(center_x, center_y, max_ring*unit);
-  fill(color(178, 34, 34, 240));
-  square(center_x, center_y, max_ring*unit);
-
+  let alpha = map(mouseX, 0, windowWidth, 210, 255);
+  fill(color(178, 34, 34, alpha));
+  square(center_x, center_y, max_ring*unit);  
   //ring of window panes
   windowlist.forEach(function(item){
     item.display();
   });
 
-   if (millis() >= 3000+timer) {
+   if (millis() >= 2000+timer) {
      regenerate();
      timer = millis();
     }
+  
+  
+  //give instructions
+  scale(-1, 1);
+  fill("white");
+  textSize(16);
+  text("options: ←, ↑, ↓, →, move mouse, mouse click", 0 ,0.88*height/2);
+  
+  fill("red");
+  ellipse(mouseX, mouseY, 50, 50);
+
 }
 
 function regenerate(){
@@ -53,15 +83,6 @@ function regenerate(){
   }
 }
 
-function clean_up_spill(){
-  fill('firebrick');
-  rectMode(CENTER);
-  let ringcount = ( max_ring -1 ) / 2 + 1;
-  rect(0,-1*ringcount*unit,windowWidth, 200);
-  rect(ringcount*unit,0,200, windowHeight);
-  rect(-1*ringcount*unit,0,200, windowHeight);
-  rect(0,ringcount*unit,windowWidth, 200);
-}
 
 function draw_stars(){
   starlist.forEach(function(star){
@@ -127,5 +148,12 @@ function keyPressed() {
   } else if (keyCode === DOWN_ARROW) {
     max_random = (max_random-1)%alpha_list.length;
     regenerate();
+  } else if (keyCode === LEFT_ARROW) {
+    imgtracker--;
+    if (imgtracker < 0) {
+      imgtracker = imglist.length - 1;
+    }
+  } else if (keyCode === RIGHT_ARROW) {
+    imgtracker++;
   }
 }
